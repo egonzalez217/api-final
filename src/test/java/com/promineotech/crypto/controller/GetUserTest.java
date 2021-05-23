3df613a9fe3f396020300c63eef364458d40c38b
Cruz Gonzalez <cruzgonzalez@Cruzs-MacBook-Air.local>
2021-05-23 15:54:45 -0700

    updated getUserTest

--- src/test/java/com/promineotech/crypto/controller/GetUserTest.java
+++ src/test/java/com/promineotech/crypto/controller/GetUserTest.java
@@ -8,11 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.promineotech.crypto.controller.support.GetUserTestSupport;
 import com.promineotech.crypto.user.entity.User;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class GetUserTest extends GetUserTestSupport {
 
 	@Test
